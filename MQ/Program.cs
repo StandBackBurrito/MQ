54ae12408aa0d0cc00738f755ce9ee9eef862675
Justin Patterson <jpatterson@healthcarepayment.com>
2017-08-06 23:19:54 -0500

    actually viewing pulses

--- MQ/Program.cs
+++ MQ/Program.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using Microsoft.SPOT;
 using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware.NetduinoPlus;
 
 namespace MQ
 {
@@ -9,12 +10,12 @@ namespace MQ
     {
         public IrEvent(long ticks, bool high)
         {
-            Ticks = ticks;
-            High = high;
+            Timestamp = ticks;
+            State = high;
         }
 
-        public long Ticks { get; set; }
-        public bool High { get; set; }
+        public long Timestamp { get; set; }
+        public bool State { get; set; }
     }
 
 
@@ -24,13 +25,10 @@ namespace MQ
         private IrEvent[] _events;
         private int _index;
         private Timer _timer;
-        private long _lastTicks;
 
         public IrReader(Cpu.Pin irIn)
         {
             _receiver = new InterruptPort(irIn, false, Port.ResistorMode.Disabled, Port.InterruptMode.InterruptEdgeBoth);
-
-            var timer = new Timer()
 
             _receiver.OnInterrupt += _receiver_OnInterrupt;
 
@@ -38,20 +36,42 @@ namespace MQ
 
             _index = 0;
 
-            _lastTicks = DateTime.Now.Ticks;
-            _timer = new Timer();
+            _timer = new Timer(new TimerCallback(RCtimeout), null, Timeout.Infinite, Timeout.Infinite);
         }
+
+        void RCtimeout(object o)
+        {
+            var bits = 0;
+            for (int i = 0; i < _events.Length - 1; i++)
+            {
+                if (_events[i + 1] != null && _events[i + 1].Timestamp != 0)
+                {
+                    bits++;
+                    var length = _events[i + 1].Timestamp - _events[i].Timestamp;
+
+                    Debug.Print("Event " + i.ToString() + ": was " + _events[i].State.ToString() + " for " + length.ToString() + "us");
+                }
+            }
+
+            Debug.Print("Received " + bits.ToString() + "bits");
+
+            _events = new IrEvent[256];
+            _index = 0;
+
+            _timer.Change(Timeout.Infinite, Timeout.Infinite);
+        }
+
 
         void _receiver_OnInterrupt(uint data1, uint data2, DateTime time)
         {
-            _events[_index++] = new IrEvent(_lastTicks - time.Ticks, data2 == 1);
-            _lastTicks = time.Ticks;
+            _events[_index++] = new IrEvent(time.Ticks / (System.TimeSpan.TicksPerMillisecond / 1000), data2 == 1);
 
-            if (_index >= 255)
+            if (_index >= _events.Length)
             {
                 _index = 0;
-                //Fire event
             }
+
+            _timer.Change(1, Timeout.Infinite); 
         }
 
     }
@@ -61,6 +81,8 @@ namespace MQ
         public static long ticks { get; set; }
         public static void Main()
         {
+
+            var reader = new IrReader(Pins.GPIO_PIN_D1);
             Thread.Sleep(Timeout.Infinite);
         }
 
